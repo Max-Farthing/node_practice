4c991dfd72e328ca2572d730c28acc30af5b290d
mafarthi <maxfarthing1@gmail.com>
2024-06-07 16:48:04 -0400

    finished section

--- 9-cookies/app.js
+++ 9-cookies/app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://max:admin@cluster0.vufgfqp.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
+
 const app = express();
+const store = new MongoDbStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,15 +26,26 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  if (req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
-});
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,21 +55,22 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://max:admin@cluster0.vufgfqp.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+    MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Max',
+            email: 'max@test.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      });
     app.listen(3000);
   })
   .catch(err => {
