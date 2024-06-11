d858acf455aac24178cd256dde55bf00fdb7ba05
mafarthi <maxfarthing1@gmail.com>
2024-06-11 11:46:28 -0400

    validation section done

--- 11-validation/routes/auth.js
+++ 11-validation/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -8,9 +10,48 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email'),
+        body('password',
+            'Incorrect password'
+        )
+            .isLength({ min: 4 })
+            .isAlphanumeric()
+    ],
+    authController.postLogin
+);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email')
+            .custom((value, { req }) => {
+                return User.findOne({ email: value })
+                    .then(userDoc => {
+                        if (userDoc) {
+                            return Promise.reject('Email exists already');
+                        }
+                    }
+                    )
+            }),
+        body('password',
+            'Please enter password with 4+ characters only numbers + letters'
+        )
+            .isLength({ min: 4 })
+            .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
