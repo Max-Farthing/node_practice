80e401e148bddab3a90616b9cc7643db50512f4b
mafarthi <maxfarthing1@gmail.com>
2024-06-14 16:18:04 -0400

    updated both sections to work together

--- 13-rest-api-basics/routes/feed.js
+++ 13-rest-api-basics/routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 
@@ -7,6 +8,35 @@ const router = express.Router();
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost)
+// POST /feed/post
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
+
+router.get('/post/:postId', feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', feedController.deletePost);
 
 module.exports = router;
